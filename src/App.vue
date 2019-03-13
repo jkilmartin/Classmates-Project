9b00a6e6ee66ad1b3cdddc1d91832997669bd064
Lyndon Fox <lyndonfox.web@gmail.com>
2019-03-13 16:56:31 +1300

    scecond time round

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <MyHeader/>
     <router-view></router-view>
    
-=======
-    <Header/>
-    <router-view></router-view>
->>>>>>> b15426fa9bacd88a4be9a6d751513672f5012119
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import MyHeader from "./components/Header.vue";
-=======
-import Header from "./components/Header";
->>>>>>> b15426fa9bacd88a4be9a6d751513672f5012119
 
 export default {
   name: "App",
   components: {
-<<<<<<< HEAD
     MyHeader,
 
-=======
-    Header
->>>>>>> b15426fa9bacd88a4be9a6d751513672f5012119
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 
 import App from "./App.vue";
 
-<<<<<<< HEAD
 import Lyndon from "./components/lyndon/Lyndon";
 
 
@@ -12,12 +11,6 @@ Vue.use(VueRouter);
 const routes = [
   { path: "/", component: Lyndon }
  
-=======
-Vue.use(VueRouter);
-
-const routes = [
-  { path: "/", component: Home },
->>>>>>> b15426fa9bacd88a4be9a6d751513672f5012119
 ];
 
 const router = new VueRouter({
